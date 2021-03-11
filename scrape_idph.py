a5265080d746e13a5e7b555906b19b3086444ec2
hackerlikecomputer <hackerlikecomputer@gmail.com>
2021-03-11 12:38:55 -0600

    update data for datawrapper

--- scrape_idph.py
+++ scrape_idph.py
@@ -21,25 +21,26 @@ else:
     archive_data = pd.DataFrame()
     last_archived_date = pd.Timestamp(1900, 1, 1)
 
-if last_updated_date > last_archived_date:
-    new_data = pd.DataFrame(data["VaccineAdministration"])
-    new_data.columns = new_data.columns.str.replace(
-        r"(?<!^)(?<!_)(?=[A-Z])", "_", regex=True
-    ).str.lower()
-    new_data["update_date"] = last_updated_date
-    new_data["administered_doses_per_100k"] = (
-        new_data.administered_count / new_data.population * 100_000
-    )
+new_data = pd.DataFrame(data["VaccineAdministration"])
+new_data.columns = new_data.columns.str.replace(
+    r"(?<!^)(?<!_)(?=[A-Z])", "_", regex=True
+).str.lower()
+new_data["update_date"] = last_updated_date
+new_data["administered_doses_per_100k"] = (
+    new_data.administered_count / new_data.population * 100_000
+)
+# datawrapper needs percents, not decimals
+new_data["pct_vaccinated_population"] = new_data.pct_vaccinated_population * 100
 
+if last_updated_date > last_archived_date:
     archive = archive_data.append(new_data).sort_values("update_date")
     archive.drop_duplicates().to_csv(archive_file, index=False)
 
-    new_data["census_county_name"] = new_data.county_name.apply(
-        lambda n: f"{n} County, IL"
-    )
-    new_data = new_data[
-        ["census_county_name"] + [c for c in new_data if c != "census_county_name"]
-    ]
-    new_data.drop_duplicates().to_csv(
-        "output/idph_vaccine_administration_data_current_by_county.csv", index=False
-    )
+# write daily data anyway
+new_data["census_county_name"] = new_data.county_name.apply(lambda n: f"{n} County, IL")
+new_data = new_data[
+    ["census_county_name"] + [c for c in new_data if c != "census_county_name"]
+]
+new_data.drop_duplicates().sort_values(
+    "pct_vaccinated_population", ascending=False
+).to_csv("output/idph_vaccine_administration_data_current_by_county.csv", index=False)
