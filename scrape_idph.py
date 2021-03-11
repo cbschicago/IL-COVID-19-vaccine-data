9351a9e05150555938dc4026801f85090e617af5
hackerlikecomputer <hackerlikecomputer@gmail.com>
2021-03-11 13:21:01 -0600

    add new output file for datawrapper graphics

--- scrape_idph.py
+++ scrape_idph.py
@@ -32,15 +32,33 @@ new_data["administered_doses_per_100k"] = (
 # datawrapper needs percents, not decimals
 new_data["pct_vaccinated_population"] = new_data.pct_vaccinated_population * 100
 
-if last_updated_date > last_archived_date:
-    archive = archive_data.append(new_data).sort_values("update_date")
-    archive.drop_duplicates().to_csv(archive_file, index=False)
+new_data = new_data.drop_duplicates().sort_values(
+    "pct_vaccinated_population", ascending=False
+)
 
-# write daily data anyway
+############### OUTPUT ###############
+
+# DAILY BY COUNTY
+archive = archive_data.append(new_data).sort_values("update_date")
+archive.drop_duplicates().to_csv(archive_file, index=False)
+
+# DAILY STATEWIDE
+statewide_archive = archive[archive.county_name == "Illinois"]
+statewide_archive.to_csv(
+    "output/idph_vaccine_administration_data_daily_statewide.csv", index=False
+)
+
+# CURRENT BY COUNTY
 new_data["census_county_name"] = new_data.county_name.apply(lambda n: f"{n} County, IL")
 new_data = new_data[
     ["census_county_name"] + [c for c in new_data if c != "census_county_name"]
 ]
-new_data.drop_duplicates().sort_values(
-    "pct_vaccinated_population", ascending=False
-).to_csv("output/idph_vaccine_administration_data_current_by_county.csv", index=False)
+new_data.to_csv(
+    "output/idph_vaccine_administration_data_current_by_county.csv", index=False
+)
+
+# CURRENT STATEWIDE
+new_data_statewide = new_data[new_data.county_name == "Illinois"]
+new_data_statewide.to_csv(
+    "output/idph_vaccine_administration_data_current_statewide.csv", index=False
+)
