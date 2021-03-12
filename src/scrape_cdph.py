e56e152aad82548e0d986b837ef5c2ca7f3724ab
hackerlikecomputer <hackerlikecomputer@gmail.com>
2021-03-12 10:51:26 -0600

    get 7 day average by zip code

--- src/scrape_cdph.py
+++ src/scrape_cdph.py
@@ -14,12 +14,29 @@ df[["date", "total_doses_daily", "total_doses_7day_avg"]].to_csv(
 # by zip code, current
 df = pd.read_csv("https://data.cityofchicago.org/resource/553k-3xzc.csv?$limit=10000")
 df["date"] = pd.to_datetime(df.date)
+
+# get 7day avg for each zip code
+dfs = []
+for z in df.zip_code.unique():
+    zdf = df[df.zip_code == z][["zip_code", "date", "total_doses_daily"]].sort_values(
+        "date"
+    )
+    zdf["7_day_avg"] = zdf.total_doses_daily.rolling(7, min_periods=1).mean()
+    dfs.append(zdf)
+
+df = df.merge(
+    pd.concat(dfs).drop_duplicates(), on=["zip_code", "date", "total_doses_daily"]
+)
+
+
 df = df[df.date == df.date.max()].fillna(
     "NULL"
 )  # datawrapper breaks if there is a null value for some reason
 df["vaccine_series_completed_percent_population"] = (
-    df.vaccine_series_completed_percent_population * 100
-)  # dw won't let me change it
+    # dw won't let me change it
+    df.vaccine_series_completed_percent_population
+    * 100
+)
 df.to_csv(
     "output/chicago_covid_vaccine_data_total_doses_zip_code_current.csv", index=False
 )
