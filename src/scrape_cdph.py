7b76c426aa6c46409136d60603aaa21e7c422263
hackerlikecomputer <hackerlikecomputer@gmail.com>
2021-03-12 01:26:50 -0600

    get chicago zip code data

--- src/scrape_cdph.py
+++ src/scrape_cdph.py
@@ -1,11 +1,18 @@
 import pandas as pd
 
+# citywide by day
+
 df = pd.read_csv("https://data.cityofchicago.org/resource/2vhs-cf6b.csv")
 df["date"] = pd.to_datetime(df.date)
 
-# citywide overall by day
 df = df.sort_values("date", ascending=True)
 df["total_doses_7day_avg"] = df.total_doses_daily.rolling(7, min_periods=1).mean()
 df[["date", "total_doses_daily", "total_doses_7day_avg"]].to_csv(
     "output/chicago_covid_vaccine_data_total_doses_daily.csv", index=False
 )
+
+# by zip code, current
+df = pd.read_csv("https://data.cityofchicago.org/resource/553k-3xzc.csv?$limit=10000")
+df["date"] = pd.to_datetime(df.date)
+df = df[df.date == df.date.max()]
+df.to_csv("output/chicago_covid_vaccine_data_total_doses_zip_code.csv", index=False)
