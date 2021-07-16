a0307856aba6e5c1322b24e3802f231a47e802da
hackerlikecomputer <hackerlikecomputer@gmail.com>
2021-07-16 15:19:29 -0500

    set default headers

--- src/scrapers/common.py
+++ src/scrapers/common.py
@@ -3,7 +3,22 @@ import requests
 import warnings
 
 
-def get_with_retry(url, headers, max_tries=5):
+idph_vaccine_request_headers = {
+    "Connection": "keep-alive",
+    "sec-ch-ua": "^\\^Google",
+    "accept": "application/json,*/*",
+    "sec-ch-ua-mobile": "?0",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
+    "Origin": "http://www.dph.illinois.gov",
+    "Sec-Fetch-Site": "cross-site",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Dest": "empty",
+    "Referer": "http://www.dph.illinois.gov/",
+    "Accept-Language": "en-US,en;q=0.9",
+}
+
+
+def get_with_retry(url, headers=idph_vaccine_request_headers, max_tries=5):
     time.sleep(1)  # always delay
     tries = 0
     while tries < max_tries:
@@ -18,18 +33,3 @@ def get_with_retry(url, headers, max_tries=5):
             return resp
     else:
         raise requests.RequestException(f"request to {url} failed")
-
-
-idph_vaccine_request_headers = {
-    "Connection": "keep-alive",
-    "sec-ch-ua": "^\\^Google",
-    "accept": "application/json,*/*",
-    "sec-ch-ua-mobile": "?0",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
-    "Origin": "http://www.dph.illinois.gov",
-    "Sec-Fetch-Site": "cross-site",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Dest": "empty",
-    "Referer": "http://www.dph.illinois.gov/",
-    "Accept-Language": "en-US,en;q=0.9",
-}
